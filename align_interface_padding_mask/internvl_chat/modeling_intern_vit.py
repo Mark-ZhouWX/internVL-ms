f5009c553f7e12572a4342f438358e38c46f4eeb
kk <hukang13@huawei.com>
2024-08-02 16:16:54 +0800

    align internvit

--- align_interface_padding_mask/internvl_chat/modeling_intern_vit.py
+++ align_interface_padding_mask/internvl_chat/modeling_intern_vit.py
@@ -1,40 +1,66 @@
 import numpy as np
 from typing import Optional, Tuple
+from einops import rearrange
 import mindspore as ms
-from mindspore import nn, ops
+from mindspore import nn, ops, Tensor
+from mindspore.numpy import ones
 
-from mindcv.models.layers.drop_path import DropPath
+from mindnlp.transformers.modeling_utils import PreTrainedModel
 
 from .configuration_intern_vit import InternVisionConfig
 
+
+has_flash_attn = False
 
 NORM2FN = {
     'layer_norm': nn.LayerNorm,
 }
 
 
+class DropPath(nn.Cell):
+    """DropPath (Stochastic Depth) regularization layers"""
+
+    def __init__(
+        self,
+        drop_prob: float = 0.0,
+        scale_by_keep: bool = True,
+    ) -> None:
+        super().__init__()
+        self.keep_prob = 1.0 - drop_prob
+        self.scale_by_keep = scale_by_keep
+        self.dropout = nn.Dropout(p=drop_prob)
+
+    def construct(self, x: Tensor) -> Tensor:
+        if self.keep_prob == 1.0 or not self.training:
+            return x
+        shape = (x.shape[0],) + (1,) * (x.ndim - 1)
+        random_tensor = self.dropout(ones(shape))
+        if not self.scale_by_keep:
+            random_tensor = ops.mul(random_tensor, self.keep_prob)
+        return x * random_tensor
+
+
 class InternVisionEmbeddings(nn.Cell):
     def __init__(self, config: InternVisionConfig):
-        super().__init__()
+        super().__init__(config)
         self.config = config
         self.embed_dim = config.hidden_size
         self.image_size = config.image_size
         self.patch_size = config.patch_size
-        self.dtype = config.compute_dtype
 
         self.class_embedding = ms.Parameter(
-            ops.randn((1, 1, self.embed_dim), dtype=self.dtype),
+            ops.randn((1, 1, self.embed_dim)),
         )
 
         self.patch_embedding = nn.Conv2d(
             in_channels=3, out_channels=self.embed_dim, kernel_size=self.patch_size, stride=self.patch_size,
-            has_bias=True, dtype=self.dtype
+            has_bias=True
         )
 
         self.num_patches = (self.image_size // self.patch_size) ** 2
         self.num_positions = self.num_patches + 1
 
-        self.position_embedding = ms.Parameter(ops.randn((1, self.num_positions, self.embed_dim), dtype=self.dtype))
+        self.position_embedding = ms.Parameter(ops.randn((1, self.num_positions, self.embed_dim)))
 
     def _get_pos_embed(self, pos_embed, H, W):
         target_dtype = pos_embed.dtype
@@ -59,7 +85,7 @@ class InternAttention(nn.Cell):
     """Multi-headed attention from 'Attention Is All You Need' paper"""
 
     def __init__(self, config: InternVisionConfig):
-        super().__init__()
+        super().__init__(config)
         self.config = config
         self.embed_dim = config.hidden_size
         self.num_heads = config.num_attention_heads
@@ -73,13 +99,13 @@ class InternAttention(nn.Cell):
             )
 
         self.scale = self.head_dim ** -0.5
-        self.qkv = nn.Dense(self.embed_dim, 3 * self.embed_dim, has_bias=config.qkv_bias, dtype=config.compute_dtype)
+        self.qkv = nn.Dense(self.embed_dim, 3 * self.embed_dim, has_bias=config.qkv_bias)
         self.attn_drop = nn.Dropout(p=config.attention_dropout)
         self.proj_drop = nn.Dropout(p=config.dropout)
 
         self.qk_normalization = config.qk_normalization
 
-        self.proj = nn.Dense(self.embed_dim, self.embed_dim, dtype=config.compute_dtype)
+        self.proj = nn.Dense(self.embed_dim, self.embed_dim)
 
     def _naive_attn(self, x):
         B, N, C = x.shape
@@ -92,7 +118,7 @@ class InternAttention(nn.Cell):
             k = self.k_norm(k.swapaxes(1, 2).flatten(-2, -1)).view(B_, N_, H_, D_).swapaxes(1, 2)
 
         attn = ((q * self.scale) @ k.swapaxes(-2, -1))
-        attn = ops.softmax(attn, axis=-1, dtype=self.config.compute_dtype)
+        attn = ops.softmax(attn, axis=-1)
         attn = self.attn_drop(attn)
 
         x = (attn @ v).swapaxes(1, 2).reshape(B, N, C)
@@ -102,7 +128,9 @@ class InternAttention(nn.Cell):
 
     def _flash_attn(self, x, key_padding_mask=None, need_weights=False):
         qkv = self.qkv(x)
-        qkv = rearrange(qkv, 'b s (three h d) -> b s three h d', three=3, h=self.num_heads)
+        # (7, 1025, 3072) --> (7, 1025, 3, 16, 64)
+        h = self.num_heads
+        qkv = ops.reshape(qkv, (qkv.shape[0], qkv.shape[1], 3, h, qkv.shape[2]//(3 * h)))
 
         if self.qk_normalization:
             q, k, v = qkv.unbind(2)
@@ -113,21 +141,23 @@ class InternAttention(nn.Cell):
         context, _ = self.inner_attn(
             qkv, key_padding_mask=key_padding_mask, need_weights=need_weights, causal=False
         )
-        outs = self.proj(rearrange(context, 'b s h d -> b s (h d)'))
+
+        b, s, h, d = context.shape
+        outs = self.proj(ops.reshape(context, (b, s, int(h * d))))
         outs = self.proj_drop(outs)
         return outs
 
     def construct(self, hidden_states: ms.Tensor) -> ms.Tensor:
-        x = self._naive_attn(hidden_states) if not self.use_flash_attn else self._flash_attn(hidden_states)
+        x = self._naive_attn(hidden_states) if not has_flash_attn else self._flash_attn(hidden_states)
         return x
 
 
 class InternMLP(nn.Cell):
     def __init__(self, config: InternVisionConfig):
-        super().__init__()
+        super().__init__(config)
         self.config = config
-        self.fc1 = nn.Dense(config.hidden_size, config.intermediate_size, dtype=config.compute_dtype)
-        self.fc2 = nn.Dense(config.intermediate_size, config.hidden_size, dtype=config.compute_dtype)
+        self.fc1 = nn.Dense(config.hidden_size, config.intermediate_size)
+        self.fc2 = nn.Dense(config.intermediate_size, config.hidden_size)
 
     def construct(self, hidden_states: ms.Tensor) -> ms.Tensor:
         hidden_states = self.fc1(hidden_states)
@@ -138,18 +168,18 @@ class InternMLP(nn.Cell):
 
 class InternVisionEncoderLayer(nn.Cell):
     def __init__(self, config: InternVisionConfig, drop_path_rate: float):
-        super().__init__()
+        super().__init__(config)
         self.embed_dim = config.hidden_size
         self.intermediate_size = config.intermediate_size
         self.norm_type = config.norm_type
 
         self.attn = InternAttention(config)
         self.mlp = InternMLP(config)
-        self.norm1 = NORM2FN[self.norm_type]([self.embed_dim], epsilon=config.layer_norm_eps, dtype=config.compute_dtype)
-        self.norm2 = NORM2FN[self.norm_type]([self.embed_dim], epsilon=config.layer_norm_eps, dtype=config.compute_dtype)
+        self.norm1 = NORM2FN[self.norm_type]([self.embed_dim], epsilon=config.layer_norm_eps)
+        self.norm2 = NORM2FN[self.norm_type]([self.embed_dim], epsilon=config.layer_norm_eps)
 
-        self.ls1 = ms.Parameter(config.initializer_factor * ops.ones(self.embed_dim, dtype=config.compute_dtype))
-        self.ls2 = ms.Parameter(config.initializer_factor * ops.ones(self.embed_dim, dtype=config.compute_dtype))
+        self.ls1 = ms.Parameter(config.initializer_factor * ops.ones(self.embed_dim))
+        self.ls2 = ms.Parameter(config.initializer_factor * ops.ones(self.embed_dim))
         self.drop_path1 = DropPath(drop_path_rate) if drop_path_rate > 0. else nn.Identity()
         self.drop_path2 = DropPath(drop_path_rate) if drop_path_rate > 0. else nn.Identity()
 
@@ -178,7 +208,7 @@ class InternVisionEncoder(nn.Cell):
     """
 
     def __init__(self, config: InternVisionConfig):
-        super().__init__()
+        super().__init__(config)
         self.config = config
         # stochastic depth decay rule
         dpr = [x.item() for x in np.linspace(0, config.drop_path_rate, config.num_hidden_layers)]
@@ -221,11 +251,10 @@ class InternVisionEncoder(nn.Cell):
         return tuple(v for v in [hidden_states, encoder_states] if v is not None)
 
 
-class InternVisionModel(nn.Cell):
+class InternVisionModel(PreTrainedModel):
     def __init__(self, config: InternVisionConfig):
 
-        super().__init__()
-        self.compute_dtype = config.compute_dtype
+        super().__init__(config)
         self.config = config
 
         self.embeddings = InternVisionEmbeddings(config)
